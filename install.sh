30be25c3e89846d33ab1ccddcde41e0e9b4dc087
Naveen <ambatinaveen24@gmail.com>
2021-04-20 17:33:20 +0530

    Brew script for downloading initial setup

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install git
+brew install tree
+
+# Install MacOS Applications
+brew install --cask firefox
+brew install --cask google-chrome
+brew install --cask sublime-text
+brew install --cask alfred
+brew install --cask rectangle
+brew install --cask day-o
+brew install --cask keepingyouawake
+brew install --cask vlc
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew install --cask font-source-code-pro
